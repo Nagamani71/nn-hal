89638d3be6c162b96025e6d9057976037fb94a64
Nagamani Chennuboina nagamani.chennuboina@intel.com <nagamani.chennuboina@intel.com>
2021-05-25 22:33:57 +0530

    Adding Quant support to Cast operation

--- ngraph_creator/operations/src/Add.cpp
+++ ngraph_creator/operations/src/Add.cpp
@@ -53,12 +53,13 @@ std::shared_ptr<ngraph::Node> Add::createNode() {
 
     auto outputNode = applyActivation(addNode, activationFn);
 
+    if (checkOutputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
+        const auto& outputIndex = sModelInfo->getOperationOutput(mNnapiOperationIndex, 0);
+        outputNode = QuantizeNode(outputNode, outputIndex, ngraph::element::u8);
+    }
+
     const auto op = sModelInfo->getOperand(mDefaultOutputIndex);
     if (op.lifetime == OperandLifeTime::MODEL_OUTPUT) {
-        if (checkOutputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
-            const auto& outputIndex = sModelInfo->getOperationOutput(mNnapiOperationIndex, 0);
-            outputNode = QuantizeNode(outputNode, outputIndex, ngraph::element::u8);
-        }
         addResultNode(mDefaultOutputIndex, outputNode);
     }
     return outputNode;
