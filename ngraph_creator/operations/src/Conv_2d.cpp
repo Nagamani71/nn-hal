820387debb0acbe608e92540d9920b82a033f15c
Nagamani Chennuboina nagamani.chennuboina@intel.com <nagamani.chennuboina@intel.com>
2021-07-07 10:19:23 +0530

    Working Per channel quant for conv, depthwise

--- ngraph_creator/operations/src/Conv_2d.cpp
+++ ngraph_creator/operations/src/Conv_2d.cpp
@@ -17,12 +17,14 @@ bool Conv_2d::validate() {
         !checkOutputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM))
         return false;
 
-    for (int i = 0; i < 2; i++) {
-        // Check input/filter operands(0/1) are of type TENSOR_FLOAT32/TENSOR_QUANT8_ASYMM
-        if (!checkInputOperandType(i, (int32_t)OperandType::TENSOR_FLOAT32) &&
-            !checkInputOperandType(i, (int32_t)OperandType::TENSOR_QUANT8_ASYMM))
-            return false;
-    }
+    // Check input/filter operands(0/1) are of type TENSOR_FLOAT32/TENSOR_QUANT8_ASYMM
+    if (!checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32) &&
+        !checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM))
+        return false;
+    if (!checkInputOperandType(1, (int32_t)OperandType::TENSOR_FLOAT32) &&
+        !checkInputOperandType(1, (int32_t)OperandType::TENSOR_QUANT8_ASYMM) &&
+        !checkInputOperandType(1, (int32_t)OperandType::TENSOR_QUANT8_SYMM_PER_CHANNEL))
+        return false;
     // Check bias type
     if (!checkInputOperandType(2, (int32_t)OperandType::TENSOR_FLOAT32) &&
         !checkInputOperandType(2, (int32_t)OperandType::TENSOR_INT32))
@@ -38,6 +40,14 @@ bool Conv_2d::validate() {
     if (!isValidInputTensor(0) || !isValidInputTensor(1)) {
         ALOGE("%s Invalid dimensions for input or filter", __func__);
         return false;
+    }
+
+    if (checkInputOperandType(1, (int32_t)OperandType::TENSOR_QUANT8_SYMM_PER_CHANNEL)) {
+        const auto& operandIndex = sModelInfo->getOperationInput(mNnapiOperationIndex, 1);
+        const auto& operand = sModelInfo->getOperand(operandIndex);
+        if (operand.extraParams.channelQuant().channelDim != 0) {
+            return false;
+        }
     }
 
     const auto& inputsSize = sModelInfo->getOperationInputsSize(mNnapiOperationIndex);
@@ -224,7 +234,29 @@ std::shared_ptr<ngraph::Node> Conv_2d::createNode() {
     filterNode = getInputNode(1);
     biasNode = getInputNode(2);
 
-    if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
+    if (checkInputOperandType(1, (int32_t)OperandType::TENSOR_QUANT8_SYMM_PER_CHANNEL)) {
+        auto filterIndex = sModelInfo->getOperationInput(mNnapiOperationIndex, 1);
+        const auto& filterOperand = sModelInfo->getOperand(filterIndex);
+        vec<float> filterScales = filterOperand.extraParams.channelQuant().scales;
+        float inputScale = sModelInfo->getOperandScale(0);
+        auto filterScales_node =
+            createConstNode(ngraph::element::f32, ngraph::Shape{filterScales.size()}, filterScales);
+        auto inputScales_node =
+            createConstNode(ngraph::element::f32, ngraph::Shape{1}, convertToVector(inputScale));
+        filterNode = transpose(NHWC_CWHN, filterNode);
+        auto filterNodeConvert =
+            std::make_shared<ngraph::opset3::Convert>(filterNode, ngraph::element::f32);
+        auto filterNodeMulScales =
+            std::make_shared<ngraph::opset3::Multiply>(filterNodeConvert, filterScales_node);
+        filterNode = transpose(CWHN_NHWC, filterNodeMulScales);
+        // for quant symm per channel type inputs, bias is of type TENSOR_INT32. For TENSOR_INT32
+        // type, dequantization is not applied during node creation
+        // bias_scale[i] = input_scale * filter_scale[i]
+        auto biasScalMultiplier =
+            std::make_shared<ngraph::opset3::Multiply>(filterScales_node, inputScales_node);
+        biasNode = std::make_shared<ngraph::opset3::Convert>(biasNode, ngraph::element::f32);
+        biasNode = std::make_shared<ngraph::opset3::Multiply>(biasNode, biasScalMultiplier);
+    } else if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
         // for quant type inputs, bias is of type TENSOR_INT32. For TENSOR_INT32 type,
         // dequantization is not applied during node creation
         biasNode = DequantizeNode(biasNode, biasIndex, ngraph::element::f32);
